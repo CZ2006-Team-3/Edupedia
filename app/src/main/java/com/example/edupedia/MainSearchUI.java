01e63e939c26f001d46ee7c4080a43ae7c9944e5
kane <45481343+kaneishere@users.noreply.github.com>
2020-03-05 19:49:06 +0800

    -test commit 2

--- app/src/main/java/com/example/edupedia/MainSearchUI.java
+++ app/src/main/java/com/example/edupedia/MainSearchUI.java
@@ -25,7 +25,7 @@ public class MainSearchUI extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        hiii;
+
     }
 
 }
