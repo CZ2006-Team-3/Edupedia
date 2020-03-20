e6f51aef9d10b7d468fb1254657c39951dd60165
Limmy97 <Limmy97@users.noreply.github.com>
2020-03-20 17:44:38 +0800

    Updated watchlist controller

--- app/src/main/java/com/example/edupedia/controller/WatchlistController.java
+++ app/src/main/java/com/example/edupedia/controller/WatchlistController.java
@@ -3,35 +3,43 @@ package com.example.edupedia.controller;
 import com.example.edupedia.model.School;
 
 public class WatchlistController {
+    // watchlist variable which hold the user's saved results
     private School[] watchList;
 
+    // static variable watchlistController  of type WatchlistController
     private static WatchlistController watchlistController = null;
 
+    // private constructor restricted to this class itself
     private WatchlistController() {
         watchList = new School[10];
     }
 
+    // Static method to create instance of Singleton class
     public static WatchlistController getInstance() {
         if (watchlistController == null)
             watchlistController = new WatchlistController();
         return watchlistController;
     }
 
+    // Getter method for retrieving watchlist
     public School[] getWatchList() {
         return watchList;
     }
 
-    void addSchool(School school, int toAdd) {
+    // Method for user to add a result to the watchlist
+    public void addSchool(School school, int toAdd) {
         watchList[toAdd] = school;
     }
 
-    void changePosition(int pos1, int pos2) {
+    // Method for user to swap 2 result positions in the watchlist
+    public void changePosition(int pos1, int pos2) {
         School temp = watchList[pos1];
         watchList[pos1] = watchList[pos2];
         watchList[pos2] = temp;
     }
 
-    void removeSchool(int toRemove) {
+    // method to remove a result from the watchlist
+    public void removeSchool(int toRemove) {
         watchList[toRemove] = null;
     }
 
