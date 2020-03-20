77bd1817fd2737ab37207b2709bc8c56f8feaf43
Limmy97 <Limmy97@users.noreply.github.com>
2020-03-20 17:14:13 +0800

    Added WatchlistController singleton class with School[] watchlist attribute.

---
+++ app/src/main/java/com/example/edupedia/controller/WatchlistController.java
@@ -0,0 +1,39 @@
+package com.example.edupedia.controller;
+
+import com.example.edupedia.model.School;
+
+public class WatchlistController {
+    private School[] watchList;
+
+    private static WatchlistController watchlistController = null;
+
+    private WatchlistController() {
+        watchList = new School[10];
+    }
+
+    public static WatchlistController getInstance() {
+        if (watchlistController == null)
+            watchlistController = new WatchlistController();
+        return watchlistController;
+    }
+
+    public School[] getWatchList() {
+        return watchList;
+    }
+
+
+    void addSchool(School school, int toAdd) {
+        watchList[toAdd] = school;
+    }
+
+    void changePosition(int pos1, int pos2) {
+        School temp = watchList[pos1];
+        watchList[pos1] = watchList[pos2];
+        watchList[pos2] = temp;
+    }
+
+    void removeSchool(int toRemove) {
+        watchList[toRemove] = null;
+    }
+
+}
