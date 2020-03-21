74ce1b03689a7b3ed2566570919f67ec407f6089
Limmy97 <Limmy97@users.noreply.github.com>
2020-03-21 10:54:23 +0800

    Updated watchlist controller

--- app/src/main/java/com/example/edupedia/controller/WatchlistController.java
+++ app/src/main/java/com/example/edupedia/controller/WatchlistController.java
@@ -1,18 +1,50 @@
 package com.example.edupedia.controller;
 
+import android.util.Log;
+
 import com.example.edupedia.model.School;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WatchlistController {
-    // watchlist variable which hold the user's saved results
-    private School[] watchList;
+    public String[] getWatchlist() {
+        return watchlist;
+    }
 
-    // static variable watchlistController  of type WatchlistController
+    public FirebaseUser getUser() {
+        return user;
+    }
+
+    public void setWatchlist(String[] watchlist) {
+        this.watchlist = watchlist;
+    }
+
+    public void setUser(FirebaseUser user) {
+        this.user = user;
+    }
+
+    // Watchlist to push to cloud
+    private String[] watchlist;
+
+    private FirebaseUser user;
+
+    // static variable watchlistController of type WatchlistController
     private static WatchlistController watchlistController = null;
 
     // private constructor restricted to this class itself
     private WatchlistController() {
-        watchList = new School[10];
+        watchlist = new String[10];
+        pullWatchlist();
     }
+
 
     // Static method to create instance of Singleton class
     public static WatchlistController getInstance() {
@@ -21,26 +53,61 @@ public class WatchlistController {
         return watchlistController;
     }
 
-    // Getter method for retrieving watchlist
-    public School[] getWatchList() {
-        return watchList;
-    }
 
     // Method for user to add a result to the watchlist
     public void addSchool(School school, int toAdd) {
-        watchList[toAdd] = school;
+        watchlist[toAdd] = school.getSchoolName();
     }
 
     // Method for user to swap 2 result positions in the watchlist
     public void changePosition(int pos1, int pos2) {
-        School temp = watchList[pos1];
-        watchList[pos1] = watchList[pos2];
-        watchList[pos2] = temp;
+        String temp = watchlist[pos1];
+        watchlist[pos1] = watchlist[pos2];
+        watchlist[pos2] = temp;
     }
 
     // method to remove a result from the watchlist
     public void removeSchool(int toRemove) {
-        watchList[toRemove] = null;
+        watchlist[toRemove] = null;
+    }
+
+
+    final String user_id = "INmxYLFnjyRM2tO8PujPc9KyFxD2";
+    DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("User_DB").child(user_id);
+
+    // Push watchlist to database
+    public void pushWatchlist() {
+        Log.d("PUSHWATCHLIST", watchlist[0]);
+        List<String> watchlist_List =  new ArrayList<>();
+        for (int i =0; i<10; i++){
+            watchlist_List.add(watchlist[i]);
+        }
+        current_user_db.child("watchlist").setValue(watchlist_List);
+    }
+
+    // Retrieve watchlist from database
+    public void pullWatchlist() {
+        current_user_db.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // ...
+                if (dataSnapshot.child("watchlist").getValue() != null) {
+                    String key = dataSnapshot.getKey();
+                    Log.d("FireBase REAADDD", key);
+                    watchlist = (String[]) dataSnapshot.child("watchlist").getValue();
+                    Log.d("FireBase REAADDD", watchlist[0]);
+                } else {
+                    watchlist = new String[10];
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // ...
+            }
+        });
+
+
     }
 
 }
