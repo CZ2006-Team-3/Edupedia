528dc081b3b832104368370211f5dbed3289bb90
Wei Jie <weighzhie>
2020-03-21 18:20:08 +0800

    Retrieve Results and Storing of Results, All school names generated when file missing

--- app/src/main/java/com/example/edupedia/controller/UserController.java
+++ app/src/main/java/com/example/edupedia/controller/UserController.java
@@ -13,8 +13,11 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserController {
 
@@ -64,6 +67,30 @@ public class UserController {
 
     public void setUser(FirebaseUser user) {
         this.user = user;
+    }
+
+    public void getName(){
+        String user_id = user.getUid();
+        DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("User_DB").child(user_id);
+
+        current_user_db.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child("watchlist").getValue() != null) {
+                    String key = dataSnapshot.getKey();
+                    Log.d("FireBase REAADDD", key);
+                    name = dataSnapshot.child("name").getValue();
+                    Log.d("FireBase REAADDD", watchlist[0]);
+                } else {
+                    watchlist = new String[10];
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // ...
+            }
+        });
     }
 
 
