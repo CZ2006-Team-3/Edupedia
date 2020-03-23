80e0f71fdca4d5dc157388731d049500bcdac4cd
kane <45481343+kaneishere@users.noreply.github.com>
2020-03-23 11:29:21 +0800

    made "Finding your next step in higher education" navigate to HomeFragment

--- app/src/main/java/com/example/edupedia/ui/SearchFragment.java
+++ app/src/main/java/com/example/edupedia/ui/SearchFragment.java
@@ -19,6 +19,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.edupedia.R;
@@ -27,6 +29,7 @@ import com.example.edupedia.controller.SortController;
 import com.example.edupedia.model.School;
 import com.example.edupedia.model.SchoolDB;
 import com.example.edupedia.controller.SearchController;
+import com.example.edupedia.ui.home.HomeFragment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -267,6 +270,9 @@ public class SearchFragment extends Fragment implements
                 //Store List of School Names
                 viewModel.storeResults(results);
                 //shift Fragment here
+                HomeFragment homeFragment = new HomeFragment();
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_container, homeFragment).commit();
             }
         });
 
